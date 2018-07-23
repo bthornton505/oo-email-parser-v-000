fe83866f96d95bfd290fb6cb4c67c0139c7c1dfe
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-23 23:00:02 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,8 +12,8 @@ class EmailParser
   
   def parse
     # I should be able to parse an email that is separated either with a comma or whitespace 
-    @emails.split.map do |email|
-      email.split(",")
-    end.flatten.uniq
-  end 
+    @emails.split.map do |email| # This first separates and maps the individual emails into an array. 
+      email.split(",") # This then takes those emails and makes sure to separate them with a comma.
+    end.flatten.uniq # This takes the newly created array of emails and flattens them into one singular array.
+  end                # Uniq makes sure that any doubles are removed.
 end 
